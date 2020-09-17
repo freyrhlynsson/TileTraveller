54fd9693c4d67d0473779e4005092d8fb4f14702
Freyr Hlynsson <freyr19@ru.is>
2020-09-17 09:36:38 +0000

    Comments on algorithm

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,6 @@
-# Write the board
+# Harðkóða inn veggi í borðið
+# Lýsa stöðu spilara sem talnapari x og y
+# Tékka í allar áttir hvort x eða y verða 0 eða 4, eða hvort þær fara í gegnum vegg
+# Skrifa út mögulegar áttir
+# Fá leifilega átt og hreyfa
+# Tékka hvenær spilari vinnur
